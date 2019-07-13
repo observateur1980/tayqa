1f820952b0bc3844eb1a0191bb4523989507dcc1
observateur1980 <44187905+observateur1980@users.noreply.github.com>
2019-07-13 00:37:53 -0700

    asas

--- tayqa/settings/production.py
+++ tayqa/settings/production.py
@@ -2,5 +2,5 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 DEBUG = False
-ALLOWED_HOSTS = [ '173.255.210.251','www.tayqa.com', 'tayqa.com']
+ALLOWED_HOSTS = [ '173.255.210.','www.tayqa.com', 'tayqa.com']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
